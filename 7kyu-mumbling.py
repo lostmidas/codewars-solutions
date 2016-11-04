2f30f9939d1e3afbc10f50b24dcd61fb73d918e6
Jason Trikakis <jason_trikakis@mac.com>
2016-11-04 10:10:13 -0700

    Add files via upload

---
+++ 7kyu-mumbling.py
@@ -0,0 +1,8 @@
+'''
+Write a function "accum(s)" that returns the following:
+accum("abcd")    # "A-Bb-Ccc-Dddd"
+accum("RqaEzty") # "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt")    # "C-Ww-Aaa-Tttt"
+'''
+def accum(s):
+    return "-".join([str(s[i] * (i + 1)).capitalize() for i in range(len(s))])
