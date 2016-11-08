7f1a2b9d2b92fed0714491b69cc2ca3ed2aed7d9
Jason Trikakis <jason_trikakis@mac.com>
2016-11-08 12:07:22 -0800

    Update 7kyu-mumbling.py

--- 7kyu-mumbling.py
+++ 7kyu-mumbling.py
@@ -4,5 +4,6 @@ accum("abcd")    # "A-Bb-Ccc-Dddd"
 accum("RqaEzty") # "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt")    # "C-Ww-Aaa-Tttt"
 '''
+
 def accum(s):
     return "-".join([str(s[i] * (i + 1)).capitalize() for i in range(len(s))])
